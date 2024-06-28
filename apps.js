afbc28d99e29f8cc45e0024493231fc7cf659135
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-06-28 18:16:40 +0300

    accordion

--- apps.js
+++ apps.js
@@ -1,6 +1,28 @@
+//menu
+
 const menuBtn = document.querySelector(".menu__btn");
 const menuMobile = document.querySelector(".header__menu-list");
 
 menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu__open");
 });
+
+
+
+// accordion
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
