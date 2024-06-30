d971a71698b95c6ac98e72069d4ebc23c1f0f774
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-06-30 23:19:42 +0300

    slider

--- apps.js
+++ apps.js
@@ -30,12 +30,12 @@ items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 // Slider
 
-const images = document.querySelectorAll('.slider img');
+const testimonials = document.querySelectorAll('.slider img');
 let currentIndex = 0;
 
-function showImage(index) {
-  images[currentIndex].classList.remove('active');
-  images[index].classList.add('active');
+function showTestimonial(index) {
+  testimonials[currentIndex].classList.remove('active');
+  testimonials[index].classList.add('active');
   currentIndex = index;
 }
 
@@ -45,16 +45,16 @@ document
     if (event.target.classList.contains('prev')) {
       let index = currentIndex - 1;
       if (index < 0) {
-        index = images.length - 1;
+        index = testimonials.length - 1;
       }
-      showImage(index);
+      showTestimonial(index);
     } else if (event.target.classList.contains('next')) {
       let index = currentIndex + 1;
-      if (index >= images.length) {
+      if (index >= testimonials.length) {
         index = 0;
       }
-      showImage(index);
+      showTestimonial(index);
     }
   });
 
-showImage(currentIndex);
+showTestimonial(currentIndex);
